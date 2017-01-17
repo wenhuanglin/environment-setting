f4a7f382215bfa56344b5ae61fb6c47a42c8ce04
wenhuanglin <ben.wenhuang.lin@gmail.com>
2017-01-17 16:07:27 +0100

    add installation script

---
+++ environment_set.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get -y install vim
+
+#Janus vim
+curl -L https://bit.ly/janus-bootstrap | bash
+
+#pathogen.vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+#zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#vim-go using pathogen
+git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+
+#tagbar for vim using pathogen
+git clone git@github.com:majutsushi/tagbar.git ~/.vim/bundle/tagbar
